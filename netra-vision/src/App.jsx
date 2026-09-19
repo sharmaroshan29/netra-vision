0274676f20777459437d6f66d8e2a3255abf82df
sharmaroshan29 <deeproshan006@gmail.com>
2026-09-20 00:13:11 +0530

    Updated API_BASE in frontend

--- netra-vision/src/App.jsx
+++ netra-vision/src/App.jsx
@@ -18,7 +18,7 @@ import {
 } from 'lucide-react';
 import HeroSection from './components/HeroSection';
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://netra-vision-crop-disease-management.onrender.com";
 
 const getValidImageUrl = (url) => {
   if (!url) return "";
